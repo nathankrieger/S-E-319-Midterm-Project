4406d58962fa371d34181a742f5e8f0c912f55b0
Roccwitz <131015732+Roccwitz@users.noreply.github.com>
2024-02-26 16:49:26 -0600

    moving files from laptop to desktop

---
+++ courses.js
@@ -0,0 +1,14 @@
+let row = `<div class="row g-4 py-5 row-cols-1 row-cols-lg-3"></div>`
+let column = `<div class="col d-flex align-items-start">
+<div>
+  <h3 class="fs-2 text-body-emphasis">${courseTitle}</h3>
+  <p>${courseDesc}</p>
+  <a href="#" class="btn btn-primary">
+    Go to course
+  </a>
+</div>
+</div>`
+
+function generateClasses() {
+    
+}
