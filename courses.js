6df49846f372dd924cc52a74eda1cdabd336b8bc
Rocco Zollo <zollorocco@gmail.com>
2024-03-07 23:20:31 -0600

    added working search bar to classes page

--- courses.js
+++ courses.js
@@ -1,33 +1,55 @@
 
+let search;
+let courseList;
 fetch("./courses.json")
-.then(response => response.json())
-.then(courses => generateCourses(courses));
+.then((courses) => courses.json())
+.then((json) => courseList = json);
 
-function generateCourses(courses) {
+window.onload = function () {
+  generateCourses(courseList, "");
+  search = document.getElementById("search");
+
+  search.addEventListener("input", () => {
+    document.getElementById("classes").remove();
+    generateCourses(courseList, search.value);
+  });
+};
+
+function generateCourses(courses, query) {
   container = document.getElementById("maincontainer");
 
   let row = document.createElement("div");
+  row.id = "classes";
   row.classList.add("row");
   row.classList.add("g-4");
-  row.classList.add("py-5");
+  row.classList.add("pb-5");
   row.classList.add("row-cols-1");
   row.classList.add("row-cols-lg-3");
 
-  for (let i = 0; i < courses[0]["courses"].length; i++) {
-    let column = document.createElement("div");
-    column.classList.add("col");
-    column.classList.add("d-flex");
-    column.classList.add("align-items-start");
-    column.innerHTML = `
-    <div>
-      <h3 class="fs-2 text-body-emphasis">${courses[0]["courses"][i]}</h3>
-      <p>${courses[0]["courses"][i]}</p>
-      <a href="#" class="btn btn-primary">
-        Go to course
-      </a>
-    </div>`;
-    row.appendChild(column);
-    
-    container.appendChild(row);
+  for (let j = 0; j < courses.length; j++) {
+    if (courses[j]["major"] == "Computer Science (COMS)") {
+      for (let i = 0; i < courses[j]["courses"].length; i++) {
+        if (courses[j]["courses"][i].includes(query)) {
+          console.log(query);
+          let column = document.createElement("div");
+          column.classList.add("col");
+          column.classList.add("d-flex");
+          column.classList.add("align-items-start");
+          column.classList.add("box");
+          column.classList.add("py-5");
+          column.innerHTML = `
+          <div>
+            <h3 class="fs-2" style="color: white;">${courses[j]["courses"][i]}</h3>
+            <p>${courses[j]["courses"][i]}</p>
+            <a href="#" class="btn btn-primary">
+              Go to course
+            </a>
+          </div>`;
+          row.appendChild(column);
+        }
+      }
+    }
   }
+
+  container.appendChild(row);
 }
