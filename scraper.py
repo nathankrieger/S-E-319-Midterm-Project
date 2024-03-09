3bef18c0f69bdfc6a3eac02f9aa50a3bd8abf201
Rocco Zollo <zollorocco@gmail.com>
2024-03-09 14:57:07 -0600

    data restructure with flowchart images

--- scraper.py
+++ scraper.py
@@ -23,12 +23,16 @@ for major in data:
     html.close()
     soup = BeautifulSoup(text, "html.parser")
 
-    print(text)
-
     courses = soup.find_all("a", class_="toggle-accordion courseblocklink open")
-    for course in courses:
-        major["courses"].append(course.text.replace("\u00a0", "").replace("\n", ""))
+    credits = soup.find_all("p", class_="credits noindent")
+    for course, credit in zip(courses, credits):
+        if "(COMS)" in major["major"] or "(CPRE)" in major["major"] or "(SE)" in major["major"]:
+            major["courses"].append({})
+            major["courses"][-1]["courseCode"] = course.text.replace("\u00a0", "").replace("\n", "").split(':')[0]
+            major["courses"][-1]["courseTitle"] = course.text.replace("\u00a0", "").replace("\n", "").split(':')[1].strip()
+            major["courses"][-1]["credits"] = credit.text.split(':')[1].split('.')[0].strip()
+            major["courses"][-1]["img"] = ""
 
-file = open("courses.json", "w")
+file = open("data.json", "w")
 file.write(json.dumps(data, indent=4))
 file.close()
